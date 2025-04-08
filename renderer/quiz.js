f72892b113b53ca1fc4516dd05c5e71aa36d78d4
Toochidennis <dennistoochukwu@gmail.com>
2025-04-08 20:31:20 +0100

    update

--- renderer/quiz.js
+++ renderer/quiz.js
@@ -1,4 +1,5 @@
 const state = require('./state');
+const axios = require('axios');
 
 const countdown = document.getElementById('countdown');
 const submitBtn = document.getElementById('submit-button');
@@ -11,7 +12,30 @@ const nextBtn = document.getElementById('next-btn');
 const prevBtn = document.getElementById('prev-btn');
 
 
-window.api.startExam((_, data) => {
+fetchQuestions();
 
-});
+function fetchQuestions() {
+    axios.get(`https://linkschoolonline.com/quiz}`)
+        .then(response => {
+            console.log(response.data);
+            formatQuestions(response.data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
 
+function formatQuestions(response) {
+    questionsData = response.map(q => ({
+        question_text: q.question_text,
+        options: [
+            { text: q.option_1_text, image: q.option_1_image },
+            { text: q.option_2_text, image: q.option_2_image },
+            { text: q.option_3_text, image: q.option_3_image },
+            { text: q.option_4_text, image: q.option_4_image }
+        ],
+        answer: q.answer,
+    }));
+
+    console.log(questionsData);
+}
